90b7e87a77be42497a4e677b7c2c3fa75555ea57
JiuMu <jiu_mu@live.cn>
2016-08-13 17:28:51 +0800

    add background login view

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var fs=require('fs');
-
-
+var fs = require('fs');
+var session = require('express-session');
+var flash = require('express-flash');
+var passport = require('passport');
 var routes = require('./routes/index');
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
 
 var app = express();
 
@@ -19,20 +28,31 @@ app.set('view engine', 'jade');
 app.use(favicon(path.join(__dirname, 'public', 'img/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({
+    secret: 'yto',
+    name: 'yto280901',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 12000000
+    }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash())
 //注册根路由
 app.use('/', routes);
 //初始化路由
-initRoutes('./routes','/');
+initRoutes('./routes', '/');
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -40,23 +60,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
@@ -66,22 +86,22 @@ module.exports = app;
 //===============华丽的分割线======================
 
 //递归初始化路由
-function initRoutes(dirPath,urlPath){
-    console.log('开始搜索文件夹'+dirPath);
-    var files= fs.readdirSync(dirPath);
-    var reg=/^(.*)\.js$/i;
-    files.forEach(function(file){
-        var tempPath=dirPath+'/'+file;
-        var stats= fs.statSync(tempPath);
-        if(stats.isDirectory()){
-            var tempUrlPath=urlPath+file+'/';
-            initRoutes(tempPath,tempUrlPath);
-        }else {
-            if(reg.test(file)){
-                console.log('发现路由文件并尝试注册:'+dirPath+'/'+file);
-                var ph=RegExp.$1;
-                app.use(urlPath+ph,require(dirPath+'/'+ph));
-                console.log('注册路由成功:'+urlPath+ph);
+function initRoutes(dirPath, urlPath) {
+    console.log('开始搜索文件夹' + dirPath);
+    var files = fs.readdirSync(dirPath);
+    var reg = /^(.*)\.js$/i;
+    files.forEach(function (file) {
+        var tempPath = dirPath + '/' + file;
+        var stats = fs.statSync(tempPath);
+        if (stats.isDirectory()) {
+            var tempUrlPath = urlPath + file + '/';
+            initRoutes(tempPath, tempUrlPath);
+        } else {
+            if (reg.test(file)) {
+                console.log('发现路由文件并尝试注册:' + dirPath + '/' + file);
+                var ph = RegExp.$1;
+                app.use(urlPath + ph, require(dirPath + '/' + ph));
+                console.log('注册路由成功:' + urlPath + ph);
             }
         }
     });
--- routes/background.js
+++ routes/background.js
@@ -1,13 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
+
+passport.use('bg_local', new LocalStrategy(
+    function (username, password, done) {
+        var user = {
+            id: '1',
+            username: 'admin',
+            password: 'admin'
+        }; // 可以配置通过数据库方式读取登陆账号
+        if (username !== user.username) {
+            return done(null, false, { message: '用户名错误' });
+        }
+        if (password !== user.password) {
+            return done(null, false, { message: '密码错误' });
+        }
+        return done(null, user);
+    }
+));
+function isAuthenticated(req,res,next){
+    if(req.isAuthenticated()) return next();
+    res.redirect('/background/singin');
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('bg_index', { title: '川藏管理区' });
+router.get('/singin', function(req, res, next) {
+    var msg='未登录,没权限';
+    if(req.query.errmsg){
+        msg=req.query.errmsg;
+    }
+  res.render('bg_singin', { msg: msg });
 });
 
-router.get('/singin',function(req,res,next){
-	res.render('bg_singin');
-});
 
+router.post('/singin',function(req,res,next){
+    passport.authenticate('bg_local',
+        function(err,user,info){
+            if(err) {return next(err);}
+            if(!user){return res.render('bg_singin',{msg:info.message});}
+            req.login(user,function(err){
+               if(err) return next(err);
+                res.redirect('index');
+            });
+        })(req,res,next);
+});
+router.all('/*',isAuthenticated);
+router.get('/index',function(req,res,next){
+    res.render('bg_index',{title:'后台管理'});
+});
 module.exports = router;
